1c01d4f1c71219ba0380c7cc0dd1516d3a65d161
Nawaz Ahmed <ahmed_nawaz@hotmail.com>
2021-02-05 16:08:56 +0000

    Item class unit tested

--- src/test/java/ItemTest.java
+++ src/test/java/ItemTest.java
@@ -1,5 +1,8 @@
 import org.junit.Before;
+import org.junit.Test;
 import stock.Item;
+
+import static org.junit.Assert.assertEquals;
 
 public class ItemTest {
 
@@ -11,5 +14,24 @@ public class ItemTest {
         item = new Item("Drum Sticks", 2.25, 9.99);
     }
 
+    @Test
+    public void canGetType() {
+        assertEquals("Drum Sticks", item.getType());
+    }
+
+    @Test
+    public void canGetPriceBought() {
+        assertEquals(2.25, item.getPriceBought(), 0.0);
+    }
+
+    @Test
+    public void canGetPriceToSell() {
+        assertEquals(9.99, item.getPriceToSell(), 0.0);
+    }
+
+    @Test
+    public void canCalculateMarkup() {
+        assertEquals(7.74, item.calculateMarkup(), 0.0);
+    }
 
 }
